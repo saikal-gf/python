bc552428bf2d1bf5e16e3ae1a36480234a7d66e2
Saikal <abdibalievasajkal5@gmail.com>
2025-05-16 23:06:35 +0600

    recipe

---
+++ main.py
@@ -0,0 +1,34 @@
+recipes = []
+
+def show_menu():
+    print("\n1. Показать все рецепты")
+    print("2. Добавить рецепт")
+    print("3. Выход")
+
+def show_recipes():
+    if not recipes:
+        print("Рецептов пока нет.")
+    else:
+        print("\nСписок рецептов:")
+        for i, recipe in enumerate(recipes, start=1):
+            print(f"{i}. {recipe}")
+
+def add_recipe():
+    title = input("Введите название рецепта: ")
+    recipes.append(title)
+    print("Рецепт добавлен!")
+
+# Главный цикл
+while True:
+    show_menu()
+    choice = input("Выберите пункт меню: ")
+
+    if choice == "1":
+        show_recipes()
+    elif choice == "2":
+        add_recipe()
+    elif choice == "3":
+        print("До свидания!")
+        break
+    else:
+        print("Неверный выбор. Попробуйте снова.")
